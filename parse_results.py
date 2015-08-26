a81826e4667d4ec195a5524ae05f5b7f972e8ce0
Marcelo Roberto Jimenez <marcelo.jimenez@gmail.com>
2015-08-26 19:31:32 -0300

    Add script to parse the results

---
+++ parse_results.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# coding: utf-8
+
+import fileinput
+import sys
+
+buffer_size = 0
+threshold = 0
+elapsed_time = 0.0
+
+for line in fileinput.input():
+    l = line.split()
+    if l:
+        if l[0] == 'IMPORTANT:':
+            if l[1] == 'Maximum':
+                pass
+            elif l[1] == 'Buffer':
+                buffer_size = int(l[3][:-1])
+                threshold = int(l[5])
+        elif l[0] == 'Elapsed':
+            elapsed_time = float(l[2])
+            print '{}, {}, {}'.format(buffer_size, threshold, elapsed_time)
+            sys.stdout.flush()
